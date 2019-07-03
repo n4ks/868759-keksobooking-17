41cd4de2f769b4bc0ada26d502d57e3b61f3bc0e
n4ks <30715874+n4ks@users.noreply.github.com>
2019-07-03 17:08:50 +0300

    в процессе написания фильтров

--- js/pin.js
+++ js/pin.js
@@ -29,19 +29,98 @@ window.pin = (function () {
 
   // Фильтрация
   var mapFiltersForm = document.querySelector('.map__filters');
-  var housingType = mapFiltersForm.querySelector('#housing-type');
+  var mapFilters = mapFiltersForm.querySelectorAll('.map__filter');
 
+  var Filter = {
+    TYPE: mapFiltersForm.querySelector('#housing-type'),
+    PRICE: mapFiltersForm.querySelector('#housing-price'),
+    ROOMS: mapFiltersForm.querySelector('#housing-rooms'),
+    GUESTS: mapFiltersForm.querySelector('#housing-guests')
+  };
   var filteredAds = [];
-  housingType.addEventListener('change', function () {
+  var onFilterChange = function () {
+    // filteredAds = ads.filter(function (ad) {
+    // return ad.offer.type === Filter.TYPE.value &&
+    //   adPrice === Filter.PRICE.value &&
+    //   ad.offer.rooms.toString() === Filter.ROOMS.value &&
+    //   ad.offer.guests.toString() === Filter.GUESTS.value;
 
-    if (housingType.value !== 'any') {
-      filteredAds = ads.filter(function (ad) {
-        return ad.offer.type === housingType.value;
-      });
-    }
+
+    var setTypeFilter = function () {
+      if (Filter.TYPE.value !== 'any') {
+        filteredAds = ads.filter(function (ad) {
+          return ad.offer.type === Filter.TYPE.value;
+        });
+      }
+    };
+    setTypeFilter();
+    var setPriceFilter = function () {
+      // var adsPrice = ad.offer.price;
+      if (Filter.PRICE.value !== 'any') {
+        filteredAds = ads.filter(function (ad) {
+          var adsPrice = ad.offer.price;
+          var priceString;
+          switch (true) {
+            case adsPrice >= 0 && adsPrice <= 10000:
+              priceString = 'low';
+              break;
+            case adsPrice >= 10000 && adsPrice <= 50000:
+              priceString = 'middle';
+              break;
+            case adsPrice >= 50000:
+              priceString = 'high';
+              break;
+            default:
+              break;
+          }
+          return priceString === Filter.PRICE.value;
+        });
+      }
+    };
+    setPriceFilter();
+    // var setRoomsFilter = function (adsRooms) {
+    //   var rooms = null;
+    //   if (adsRooms !== 'any') {
+    //     rooms = adsRooms;
+    //   }
+    //   return rooms;
+    // };
+
+    // var setGuestsFilter = function (adsGuests) {
+    //   var guests = null;
+    //   if (adsGuests !== 'any') {
+    //     guests = adsGuests;
+    //   }
+    //   return guests;
+    // };
+
+    // for (ad in ads) {
+    //   if (ads.hasOwnProperty(ad)) {
+
+    //   }
+    // };
+
+    // return setTypeFilter(ad.offer.type) === Filter.TYPE.value &&
+    //   setPriceFilter(ad.offer.price) === Filter.TYPE.value &&
+    //   setRoomsFilter(ad.offer.rooms) === Filter.ROOMS.value &&
+    //   setGuestsFilter(ad.offer.guests) === Filter.GUESTS.value;
+    // });
+
     window.pin.renderPins(filteredAds);
-  });
+  };
 
+  // Вешаем фильтры на все инпуты
+  var setFilters = function () {
+    var filters = Array.from(mapFilters);
+    filters.forEach(function (filter) {
+      filter.addEventListener('change', onFilterChange);
+    });
+  };
+  setFilters();
+
+  // Filter.TYPE.addEventListener('change', onTypeFilterChange);
+  // Filter.ROOMS.addEventListener('change', onTypeFilterChange);
+  // Filter.GUESTS.addEventListener('change', onTypeFilterChange);
   return {
     renderPins: function (adsList) {
       // Ищем существующие отрисованные пины и удаляем их из DOM
@@ -62,9 +141,6 @@ window.pin = (function () {
       };
 
       window.backend.load(successCallback, errorCallback);
-    },
-    // getAds: function () {
-    //   // return ads;
-    // }
+    }
   };
 }());
